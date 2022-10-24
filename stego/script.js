a6ce8bfc7646937d8a4f88b30adc595dac10576a
avc285 <avc285@gmail.com>
2022-10-24 08:48:50 +0100

    format poem, add decryption function to JS, update page accordingly

--- stego/script.js
+++ stego/script.js
@@ -1,128 +1,128 @@
-const poem = `Once upon a midnight dreary, while I pondered, weak and weary,
-Over many a quaint and curious volume of forgotten lore—
-    While I nodded, nearly napping, suddenly there came a tapping,
-As of some one gently rapping, rapping at my chamber door.
-“’Tis some visitor,” I muttered, “tapping at my chamber door—
-            Only this and nothing more.”
-
-    Ah, distinctly I remember it was in the bleak December;
-And each separate dying ember wrought its ghost upon the floor.
-    Eagerly I wished the morrow;—vainly I had sought to borrow
-    From my books surcease of sorrow—sorrow for the lost Lenore—
-For the rare and radiant maiden whom the angels name Lenore—
-            Nameless here for evermore.
-
-    And the silken, sad, uncertain rustling of each purple curtain
-Thrilled me—filled me with fantastic terrors never felt before;
-    So that now, to still the beating of my heart, I stood repeating
-    “’Tis some visitor entreating entrance at my chamber door—
-Some late visitor entreating entrance at my chamber door;—
-            This it is and nothing more.”
-
-    Presently my soul grew stronger; hesitating then no longer,
-“Sir,” said I, “or Madam, truly your forgiveness I implore;
-    But the fact is I was napping, and so gently you came rapping,
-    And so faintly you came tapping, tapping at my chamber door,
-That I scarce was sure I heard you”—here I opened wide the door;—
-            Darkness there and nothing more.
-
-    Deep into that darkness peering, long I stood there wondering, fearing,
-Doubting, dreaming dreams no mortal ever dared to dream before;
-    But the silence was unbroken, and the stillness gave no token,
-    And the only word there spoken was the whispered word, “Lenore?”
-This I whispered, and an echo murmured back the word, “Lenore!”—
-            Merely this and nothing more.
-
-    Back into the chamber turning, all my soul within me burning,
-Soon again I heard a tapping somewhat louder than before.
-    “Surely,” said I, “surely that is something at my window lattice;
-      Let me see, then, what thereat is, and this mystery explore—
-Let my heart be still a moment and this mystery explore;—
-            ’Tis the wind and nothing more!”
-
-    Open here I flung the shutter, when, with many a flirt and flutter,
-In there stepped a stately Raven of the saintly days of yore;
-    Not the least obeisance made he; not a minute stopped or stayed he;
-    But, with mien of lord or lady, perched above my chamber door—
-Perched upon a bust of Pallas just above my chamber door—
-            Perched, and sat, and nothing more.
-
-Then this ebony bird beguiling my sad fancy into smiling,
-By the grave and stern decorum of the countenance it wore,
-“Though thy crest be shorn and shaven, thou,” I said, “art sure no craven,
-Ghastly grim and ancient Raven wandering from the Nightly shore—
-Tell me what thy lordly name is on the Night’s Plutonian shore!”
-            Quoth the Raven “Nevermore.”
-
-    Much I marvelled this ungainly fowl to hear discourse so plainly,
-Though its answer little meaning—little relevancy bore;
-    For we cannot help agreeing that no living human being
-    Ever yet was blessed with seeing bird above his chamber door—
-Bird or beast upon the sculptured bust above his chamber door,
-            With such name as “Nevermore.”
-
-    But the Raven, sitting lonely on the placid bust, spoke only
-That one word, as if his soul in that one word he did outpour.
-    Nothing farther then he uttered—not a feather then he fluttered—
-    Till I scarcely more than muttered “Other friends have flown before—
-On the morrow he will leave me, as my Hopes have flown before.”
-            Then the bird said “Nevermore.”
-
-    Startled at the stillness broken by reply so aptly spoken,
-“Doubtless,” said I, “what it utters is its only stock and store
-    Caught from some unhappy master whom unmerciful Disaster
-    Followed fast and followed faster till his songs one burden bore—
-Till the dirges of his Hope that melancholy burden bore
-            Of ‘Never—nevermore’.”
-
-    But the Raven still beguiling all my fancy into smiling,
-Straight I wheeled a cushioned seat in front of bird, and bust and door;
-    Then, upon the velvet sinking, I betook myself to linking
-    Fancy unto fancy, thinking what this ominous bird of yore—
-What this grim, ungainly, ghastly, gaunt, and ominous bird of yore
-            Meant in croaking “Nevermore.”
-
-    This I sat engaged in guessing, but no syllable expressing
-To the fowl whose fiery eyes now burned into my bosom’s core;
-    This and more I sat divining, with my head at ease reclining
-    On the cushion’s velvet lining that the lamp-light gloated o’er,
-But whose velvet-violet lining with the lamp-light gloating o’er,
-            She shall press, ah, nevermore!
-
-    Then, methought, the air grew denser, perfumed from an unseen censer
-Swung by Seraphim whose foot-falls tinkled on the tufted floor.
-    “Wretch,” I cried, “thy God hath lent thee—by these angels he hath sent thee
-    Respite—respite and nepenthe from thy memories of Lenore;
-Quaff, oh quaff this kind nepenthe and forget this lost Lenore!”
-            Quoth the Raven “Nevermore.”
-
-    “Prophet!” said I, “thing of evil!—prophet still, if bird or devil!—
-Whether Tempter sent, or whether tempest tossed thee here ashore,
-    Desolate yet all undaunted, on this desert land enchanted—
-    On this home by Horror haunted—tell me truly, I implore—
-Is there—is there balm in Gilead?—tell me—tell me, I implore!”
-            Quoth the Raven “Nevermore.”
-
-    “Prophet!” said I, “thing of evil!—prophet still, if bird or devil!
-By that Heaven that bends above us—by that God we both adore—
-    Tell this soul with sorrow laden if, within the distant Aidenn,
-    It shall clasp a sainted maiden whom the angels name Lenore—
-Clasp a rare and radiant maiden whom the angels name Lenore.”
-            Quoth the Raven “Nevermore.”
-
-    “Be that word our sign of parting, bird or fiend!” I shrieked, upstarting—
-“Get thee back into the tempest and the Night’s Plutonian shore!
-    Leave no black plume as a token of that lie thy soul hath spoken!
-    Leave my loneliness unbroken!—quit the bust above my door!
-Take thy beak from out my heart, and take thy form from off my door!”
-            Quoth the Raven “Nevermore.”
-
-    And the Raven, never flitting, still is sitting, still is sitting
-On the pallid bust of Pallas just above my chamber door;
-And his eyes have all the seeming of a demon’s that is dreaming,
-And the lamp-light o’er him streaming throws his shadow on the floor;
-And my soul from out that shadow that lies floating on the floor
-Shall be lifted—nevermore!`;
+const poem = `<span style="font-family:'Bell MT';font-weight:normal;font-style:italic;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Once upon a midnight dreary, while I pondered, weak and weary,<br>
+Over many a quaint and curious volume of forgotten lore&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;While I nodded, nearly napping, suddenly there came a tapping,<br>
+As of some one gently rapping, rapping at my chamber door.<br>
+&ldquo;&rsquo;Tis some visitor,&rdquo; I muttered, &ldquo;tapping at my chamber door&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Only this and nothing more.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ah, distinctly I remember it was in the bleak December;<br>
+And each separate dying ember wrought its ghost upon the floor.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Eagerly I wished the morrow;&mdash;vainly I had sought to borrow<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;From my books surcease of sorrow&mdash;sorrow for the lost Lenore&mdash;<br>
+For the rare and radiant maiden whom the angels name Lenore&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nameless here for evermore.<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;And the silken, sad, uncertain rustling of each purple curtain<br>
+Thrilled me&mdash;filled me with fantastic terrors never felt before;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;So that now, to still the beating of my heart, I stood repeating<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&ldquo;&rsquo;Tis some visitor entreating entrance at my chamber door&mdash;<br>
+Some late visitor entreating entrance at my chamber door;&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This it is and nothing more.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Presently my soul grew stronger; hesitating then no longer,<br>
+&ldquo;Sir,&rdquo; said I, &ldquo;or Madam, truly your forgiveness I implore;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;But the fact is I was napping, and so gently you came rapping,<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;And so faintly you came tapping, tapping at my chamber door,<br>
+That I scarce was sure I heard you&rdquo;&mdash;here I opened wide the door;&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Darkness there and nothing more.<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Deep into that darkness peering, long I stood there wondering, fearing,<br>
+Doubting, dreaming dreams no mortal ever dared to dream before;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;But the silence was unbroken, and the stillness gave no token,<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;And the only word there spoken was the whispered word, &ldquo;Lenore?&rdquo;<br>
+This I whispered, and an echo murmured back the word, &ldquo;Lenore!&rdquo;&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Merely this and nothing more.<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Back into the chamber turning, all my soul within me burning,<br>
+Soon again I heard a tapping somewhat louder than before.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&ldquo;Surely,&rdquo; said I, &ldquo;surely that is something at my window lattice;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Let me see, then, what thereat is, and this mystery explore&mdash;<br>
+Let my heart be still a moment and this mystery explore;&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&rsquo;Tis the wind and nothing more!&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Open here I flung the shutter, when, with many a flirt and flutter,<br>
+In there stepped a stately Raven of the saintly days of yore;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Not the least obeisance made he; not a minute stopped or stayed he;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;But, with mien of lord or lady, perched above my chamber door&mdash;<br>
+Perched upon a bust of Pallas just above my chamber door&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Perched, and sat, and nothing more.<br>
+<br>
+Then this ebony bird beguiling my sad fancy into smiling,<br>
+By the grave and stern decorum of the countenance it wore,<br>
+&ldquo;Though thy crest be shorn and shaven, thou,&rdquo; I said, &ldquo;art sure no craven,<br>
+Ghastly grim and ancient Raven wandering from the Nightly shore&mdash;<br>
+Tell me what thy lordly name is on the Night&rsquo;s Plutonian shore!&rdquo;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Quoth the Raven &ldquo;Nevermore.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Much I marvelled this ungainly fowl to hear discourse so plainly,<br>
+Though its answer little meaning&mdash;little relevancy bore;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For we cannot help agreeing that no living human being<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ever yet was blessed with seeing bird above his chamber door&mdash;<br>
+Bird or beast upon the sculptured bust above his chamber door,<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;With such name as &ldquo;Nevermore.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;But the Raven, sitting lonely on the placid bust, spoke only<br>
+That one word, as if his soul in that one word he did outpour.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nothing farther then he uttered&mdash;not a feather then he fluttered&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Till I scarcely more than muttered &ldquo;Other friends have flown before&mdash;<br>
+On the morrow he will leave me, as my Hopes have flown before.&rdquo;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Then the bird said &ldquo;Nevermore.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Startled at the stillness broken by reply so aptly spoken,<br>
+&ldquo;Doubtless,&rdquo; said I, &ldquo;what it utters is its only stock and store<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Caught from some unhappy master whom unmerciful Disaster<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Followed fast and followed faster till his songs one burden bore&mdash;<br>
+Till the dirges of his Hope that melancholy burden bore<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Of &lsquo;Never&mdash;nevermore&rsquo;.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;But the Raven still beguiling all my fancy into smiling,<br>
+Straight I wheeled a cushioned seat in front of bird, and bust and door;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Then, upon the velvet sinking, I betook myself to linking<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fancy unto fancy, thinking what this ominous bird of yore&mdash;<br>
+What this grim, ungainly, ghastly, gaunt, and ominous bird of yore<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Meant in croaking &ldquo;Nevermore.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This I sat engaged in guessing, but no syllable expressing<br>
+To the fowl whose fiery eyes now burned into my bosom&rsquo;s core;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This and more I sat divining, with my head at ease reclining<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;On the cushion&rsquo;s velvet lining that the lamp-light gloated o&rsquo;er,<br>
+But whose velvet-violet lining with the lamp-light gloating o&rsquo;er,<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;She shall press, ah, nevermore!<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Then, methought, the air grew denser, perfumed from an unseen censer<br>
+Swung by Seraphim whose foot-falls tinkled on the tufted floor.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&ldquo;Wretch,&rdquo; I cried, &ldquo;thy God hath lent thee&mdash;by these angels he hath sent thee<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Respite&mdash;respite and nepenthe from thy memories of Lenore;<br>
+Quaff, oh quaff this kind nepenthe and forget this lost Lenore!&rdquo;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Quoth the Raven &ldquo;Nevermore.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&ldquo;Prophet!&rdquo; said I, &ldquo;thing of evil!&mdash;prophet still, if bird or devil!&mdash;<br>
+Whether Tempter sent, or whether tempest tossed thee here ashore,<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Desolate yet all undaunted, on this desert land enchanted&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;On this home by Horror haunted&mdash;tell me truly, I implore&mdash;<br>
+Is there&mdash;is there balm in Gilead?&mdash;tell me&mdash;tell me, I implore!&rdquo;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Quoth the Raven &ldquo;Nevermore.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&ldquo;Prophet!&rdquo; said I, &ldquo;thing of evil!&mdash;prophet still, if bird or devil!<br>
+By that Heaven that bends above us&mdash;by that God we both adore&mdash;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tell this soul with sorrow laden if, within the distant Aidenn,<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;It shall clasp a sainted maiden whom the angels name Lenore&mdash;<br>
+Clasp a rare and radiant maiden whom the angels name Lenore.&rdquo;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Quoth the Raven &ldquo;Nevermore.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&ldquo;Be that word our sign of parting, bird or fiend!&rdquo; I shrieked, upstarting&mdash;<br>
+&ldquo;Get thee back into the tempest and the Night&rsquo;s Plutonian shore!<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Leave no black plume as a token of that lie thy soul hath spoken!<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Leave my loneliness unbroken!&mdash;quit the bust above my door!<br>
+Take thy beak from out my heart, and take thy form from off my door!&rdquo;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Quoth the Raven &ldquo;Nevermore.&rdquo;<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;And the Raven, never flitting, still is sitting, still is sitting<br>
+On the pallid bust of Pallas just above my chamber door;<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;And his eyes have all the seeming of a demon&rsquo;s that is dreaming,<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;And the lamp-light o&rsquo;er him streaming throws his shadow on the floor;<br>
+And my soul from out that shadow that lies floating on the floor<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Shall be lifted&mdash;nevermore!</span>`;
 
 const outputPara = document.querySelector("#output");
 
@@ -154,6 +154,7 @@ function encrypt(poem) {
         }
         charArray.push(binCharCode);
     }
+    // console.log(charArray);
 
     const origImageData = encryptCanvasCtx.getImageData(0, 0, encryptCanvas.width, encryptCanvas.height).data;
     const encodedImageData = encryptCanvasCtx.createImageData(encryptCanvas.width, encryptCanvas.height);
@@ -192,13 +193,42 @@ function encrypt(poem) {
                 }
             }
         }
-
     }
+
     console.log(encryptCanvasCtx.getImageData(0, 0, encryptCanvas.width, encryptCanvas.height).data);
+    console.log(encryptCanvasCtx.getImageData(0, 0, encryptCanvas.width, encryptCanvas.height).data);
+
     encryptCanvasCtx.putImageData(encodedImageData, 0, 0);
-    console.log(encryptCanvasCtx.getImageData(0, 0, encryptCanvas.width, encryptCanvas.height).data);
 
     outputPara.innerHTML = displayImageData(encryptCanvasCtx.getImageData(0, 0, encryptCanvas.width, encryptCanvas.height).data);
     document.querySelector("#encrypt").disabled = true;
     document.querySelector("#decrypt").disabled = false;
+    // document.querySelector("#decrypt").onclick = decrypt(encryptCanvasCtx.getImageData(0, 0, encryptCanvas.width, encryptCanvas.height).data);
+}
+
+function decrypt(imageDataArray) {
+    // loop through RGB values, nine at a time
+    // retrieve charcodes in array
+    // convert to decimal string
+    // convert to Number
+    // convert to character
+
+    const output = [];
+    for (let i = 0; i < imageDataArray.length; i += 10) {
+        let binaryCode = "";
+        for (let j = 0; j < 8; ++j) {
+            if (imageDataArray[i + j] % 2 === 0) {
+                binaryCode += "0";
+            } else {
+                binaryCode += "1";
+            }
+        }
+        // For some reason I can't quite figure out, this break statement needs to come before the push, otherwise a single extra character will be added to the end of the poem
+        if (imageDataArray[i + 9] % 2 !== 0) {
+            break;
+        }
+        output.push(String.fromCharCode(parseInt(binaryCode, 2)));
+    }
+    console.log(output);
+    outputPara.innerHTML = output.join("");
 }
